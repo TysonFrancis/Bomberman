2b7e55ada6f4bbdd66d29c8871f06757cd05f56e
HenryS27 <steelehr@icloud.com>
2026-02-03 11:46:34 -0600

    Rough framework for bomb

--- Bomberman/Bomb.cpp
+++ Bomberman/Bomb.cpp
@@ -1,1 +1,46 @@
 #include "Bomb.h"
+
+Bomb::Bomb(float xPosition, float yPosition, bool timer,
+	int pods [10][10] )
+{
+	//timer is bool that will show if the player has the remote control power up
+	if (timer)
+	{
+		sf::sleep(sf::seconds(3.0f));
+		explode(pods);
+	}
+}
+
+
+Bomb::~Bomb() {}
+
+
+void Bomb::explode(int pods [10][10])
+{
+	//Check if the squares in each direction will stop explosion
+	do
+	{
+		pods[xPosition++][yPosition] = 0; //Pod at this location becomes dangerous
+		distance--;
+		//while statement checks if next location has a wall or if distance is 0
+	} while (pods[xPosition++][yPosition] != 1 && distance > 0);
+
+	//repeat for each direction
+	do
+	{
+		pods[xPosition--][yPosition] = 0;
+		distance--;
+	} while (pods[xPosition--][yPosition] != 1 && distance > 0);
+
+	do
+	{
+		pods[xPosition][yPosition++] = 0;
+		distance--;
+	} while (pods[xPosition][yPosition++] != 1 && distance > 0);
+
+	do
+	{
+		pods[xPosition][yPosition--] = 0;
+		distance--;
+	} while (pods[xPosition][yPosition--] != 1 && distance > 0);
+}
--- Bomberman/Bomb.h
+++ Bomberman/Bomb.h
@@ -4,10 +4,12 @@
 class Bomb
 {
 public:
-	Bomb();
+	Bomb(float, float, bool, int[10][10]);
 	~Bomb();
-	
+	void explode(int[10][10]);
+
 private:
 	int distance;
+	int xPosition;
+	int yPosition;
 };
-
