78e444d5ddb6b959f3c8b854aea08cdcb636fc8d
Dylan Fletcher <26DJF01@stu.cpsk12.org>
2026-02-09 12:08:39 -0600

    Fix tile class override error

--- Bomberman/Tile.cpp
+++ Bomberman/Tile.cpp
@@ -4,25 +4,15 @@ Tile::Tile(int type) : obstruction(false)
 {
 	switch (type)
 	{
-		case HARD_WALL:
-		case SOFT_WALL:
-		case BOMB:
-			obstruction = true;
-		case BOMB:
-			obstruction = true;
-			break;
-		case BOMB:
-			obstruction = true;
-			break;
-		case BOMB:
-			obstruction = true;
-			break;
-		case BOMB:
-			obstruction = true;
-			break;
-		case BOMB:
-			obstruction = true;
-			break;
+	case HARD_WALL:
+	case SOFT_WALL:
+	case BOMB:
+		obstruction = true;
+		break;
+	default:
+		// unreachable but compiler won't stfu about uninitialized variables
+		// if anyone knows how to suppress that please do so we can delete this
+		obstruction = false;
 	}
 }
 
