440f9a378b3903e716a39fb80e29783acfd4ce62
BigEmster <BigEmster8@outlook.com>
2026-03-12 12:00:24 -0500

    More WIP code I'll finish someday

--- Bomberman/Text.cpp
+++ Bomberman/Text.cpp
@@ -1,14 +1,51 @@
 #include "Text.h"
 
-Text::Text(string text)
+Text::Text(string text, int color, sf::Vector2f position)
 {
-	for (int i = 0; i < text.size(); i++)
+	int colorOffset;
+	sf::Vector2i glyphPosition;
+
+	// load texture from file
+	if (!texture.loadFromFile("Textures/Title.png"))
+		std::cerr << "Error loading text glyphs from Title.png!";
+
+	// choose color
+	switch (color)
 	{
-		/*
-			tolower() input
-			decide color
-			decide position
-			pull from file
-		*/
+	case 0:
+		colorOffset = 16;	// black text
+		break;
+	case 1:
+	default:
+		colorOffset = 0;	// gray text (default)
 	}
+
+	// validate text input - TODO: allow cursor character
+	for (int i = 0; i < text.size(); i++)				// for every character to display
+	{
+		if (text.at(i) > 64 && text.at(i) < 91)			// if character is uppercase between A-Z
+			text.at(i) = tolower(text.at(i));			// lowercase it
+		else if (text.at(i) < 97 || text.at(i) > 122)	// else if character is not lowercase between a-z
+			if (text.at(i) < 48 || text.at(i) > 57)		// and NaN
+				if (text.at(i) != 62)					// and not '>'
+					text.erase(i--);					// remove it, decrementing i to avoid skipping chars
+	}
+
+	// choose glyph
+	glyphPosition = {0, 0};
+	
+	// create sprite
+	sprites.push_back(new sf::Sprite(texture));
+	sprites.at(sprites.size() - 1)->setTextureRect(sf::IntRect(glyphPosition, sf::Vector2i(8, 8)));
+
+	// position sprite
+	position.x;
+	position.y;
+}
+
+
+Text::~Text()
+{
+	for (int i = 0; i < sprites.size(); i++)
+		delete sprites.at(i);
 }
--- Bomberman/Text.h
+++ Bomberman/Text.h
@@ -1,5 +1,7 @@
 #pragma once
-#include <string>
+#include <iostream>
+#include <vector>
+#include "SFML/Graphics.hpp"
 
 using std::string;
 
@@ -9,5 +11,15 @@ Only supports A-Z. Displays in caps regardless of input.*/
 class Text
 {
 public:
-	Text(string);
+	/*
+		@param std::string: the text to display on screen
+		@param int: the color of the text's drop shadow - 0 for black, 1 for gray
+		@param sf::Vector2f: The position the text should appear in - use -1 for center value
+	*/
+	Text(string, int, sf::Vector2f);
+	~Text();
+
+private:
+	sf::Texture texture;
+	std::vector<sf::Sprite*> sprites;
 };
